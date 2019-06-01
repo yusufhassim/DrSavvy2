cba185a553ff02ddfd58eeb99d7781bfb460c764
Yusuf Hassim <hassim.yusuf@gmail.com>
2019-06-01 12:57:27 +0200

    Add project files.

---
+++ DrSavvy2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DrSavvy2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DrSavvy2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DrSavvy2.WebApiApplication" Language="C#" %>
