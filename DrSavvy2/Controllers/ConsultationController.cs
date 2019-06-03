77ca3ed695d41bc9651982f4f9f0b45db7ea41f7
Yusuf Hassim <hassim.yusuf@gmail.com>
2019-06-03 08:01:44 +0200

    Added blank record consultation page

---
+++ DrSavvy2/Controllers/ConsultationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrSavvy2.Controllers
+{
+    public class ConsultationController : Controller
+    {
+        // GET: Consultation
+        public ActionResult RecordConsultation()
+        {
+            ViewBag.Title = "Record Consultation";
+            return View();
+        }
+    }
+}
