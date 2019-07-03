7cf853d52148f48dbca16e76e287d424f7886a86
Tvdw3 <u17203989@tuks.co.za>
2019-07-03 03:44:34 +0200

    All Changes

--- DrSavvy2/Controllers/AdminController.cs
+++ DrSavvy2/Controllers/AdminController.cs
@@ -49,5 +49,115 @@ namespace DrSavvy2.Controllers
         {
             return View();
         }
+        
+        public ActionResult SearchMedication()
+        {
+            return View();
+        }
+        
+        public ActionResult ModifyMedication()
+        {
+            return View();
+        }
+        public ActionResult AddMedication()
+        {
+            return View();
+        }
+        public ActionResult SearchMedType()
+        {
+            return View();
+        }
+        public ActionResult ModifyMedType()
+        {
+            return View();
+        }
+        public ActionResult AddMedType()
+        {
+            return View();
+        }
+        public ActionResult SearchInstitute()
+        {
+            return View();
+        }
+        public ActionResult ModifyInstitute()
+        {
+            return View();
+        }
+        public ActionResult AddInstitute()
+        {
+            return View();
+        }
+        public ActionResult SearchInstType()
+        {
+            return View();
+        }
+        public ActionResult ModifyInstType()
+        {
+            return View();
+        }
+        public ActionResult AddInstType()
+        {
+            return View();
+        }
+        public ActionResult SearchProcedure()
+        {
+            return View();
+        }
+        public ActionResult ModifyProcedure()
+        {
+            return View();
+        }
+        public ActionResult AddProcedure()
+        {
+            return View();
+        }
+        public ActionResult SearchMedCompany()
+        {
+            return View();
+        }
+        public ActionResult ModifyMedCompany()
+        {
+            return View();
+        }
+        public ActionResult AddMedCompany()
+        {
+            return View();
+        }
+        public ActionResult SearchMedScheme()
+        {
+            return View();
+        }
+        public ActionResult ModifyMedScheme()
+        {
+            return View();
+        }
+        public ActionResult AddMedScheme()
+        {
+            return View();
+        }
+        public ActionResult SearchXrayType()//start here
+        {
+            return View();
+        }
+        public ActionResult ModifyXrayType()
+        {
+            return View();
+        }
+        public ActionResult AddXrayType()
+        {
+            return View();
+        }
+        public ActionResult SearchBloodType()
+        {
+            return View();
+        }
+        public ActionResult ModifyBloodType()
+        {
+            return View();
+        }
+        public ActionResult AddBloodType()
+        {
+            return View();
+        }
     }
 }
